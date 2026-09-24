133e19eb2439173637c2a1d19c887c8f69db1dfc
Damien Seguin <dmnsgn@gmail.com>
2026-09-24 09:46:32 +0100

    build: update snowdev files

--- snowdev.config.js
+++ snowdev.config.js
@@ -3,6 +3,7 @@ import { basename } from "node:path";
 const packageName = basename(process.cwd());
 
 export default {
+  files: "{*.+(j|t|mj|mt|cj|ct)s,chunks/*.+(j|t|mj|mt|cj|ct)s}",
   commitAndTagVersion: {
     tagPrefix: `${packageName}@v`,
     npmPublishHint: `npm publish --workspace ${packageName}`,
